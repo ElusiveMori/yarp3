34d74fb5b51f358fdfae355b68b43d8c51b165b3
mori <mori@reu.moe>
2020-03-07 07:48:40 +0400

    wew

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    root: true,
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+        "prettier/@typescript-eslint"
+    ],
+    parser: "@typescript-eslint/parser",
+    rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-namespace": "off",
+        "no-unexpected-multiline": "error",
+        "no-extra-semi": "off",
+        "no-inner-declarations": "off",
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/camelcase": "off",
+        "prefer-const" : ["error", {destructuring: "all"}],
+        "prettier/prettier" : "warn"
+    },
+}
